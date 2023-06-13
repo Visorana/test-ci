c13468a792ebe6feaecc8b39ad7257c8ee8c656e
Viola Pavlova <violacyrus96@Gmail.com>
2023-06-13 19:03:50 +0300

    appveyor added

--- src/__tests__/healthIndicator.js
+++ src/__tests__/healthIndicator.js
@@ -1,13 +1,12 @@
 import getHealthStatus from '../healthIndicator';
 
 test.each([
-    [{ name: 'Mage', health: 90 }, 'healthy'],
-    [{ name: 'Warrior', health: 50 }, 'wounded'],
-    [{ name: 'Rogue', health: 30 }, 'wounded'],
-    [{ name: 'Paladin', health: 14 }, 'critical'],
-    [{ name: 'Wizard', health: 5 }, 'critical'],
-    ])
-    ('when character is %p returns %s', (character, expected) => {
-    const result = getHealthStatus(character);
-    expect(result).toBe(expected);
+  [{ name: 'Mage', health: 90 }, 'healthy'],
+  [{ name: 'Warrior', health: 50 }, 'wounded'],
+  [{ name: 'Rogue', health: 30 }, 'wounded'],
+  [{ name: 'Paladin', health: 14 }, 'critical'],
+  [{ name: 'Wizard', health: 5 }, 'critical'],
+])('when character is %p returns %s', (character, expected) => {
+  const result = getHealthStatus(character);
+  expect(result).toBe(expected);
 });
