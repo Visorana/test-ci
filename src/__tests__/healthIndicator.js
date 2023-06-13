28e1177694f9cb3ad67460477b03303170d2e596
Viola Pavlova <violacyrus96@Gmail.com>
2023-06-13 18:30:29 +0300

    added files

---
+++ src/__tests__/healthIndicator.js
@@ -0,0 +1,13 @@
+import getHealthStatus from '../healthIndicator';
+
+test.each([
+    [{ name: 'Mage', health: 90 }, 'healthy'],
+    [{ name: 'Warrior', health: 50 }, 'wounded'],
+    [{ name: 'Rogue', health: 30 }, 'wounded'],
+    [{ name: 'Paladin', health: 14 }, 'critical'],
+    [{ name: 'Wizard', health: 5 }, 'critical'],
+    ])
+    ('when character is %p returns %s', (character, expected) => {
+    const result = getHealthStatus(character);
+    expect(result).toBe(expected);
+});
